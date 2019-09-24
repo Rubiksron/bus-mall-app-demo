ecf2cc4fe9463e9c2d214e617daadf61d71edc36
Rubiksron <rubiksron@gmail.com>
2019-09-23 21:22:12 -0700

    implimented temp var in last for loop to clean the code up. Both the simpler and more complex demo versions of the no duplicates function are working.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //global variables
 Product.names = ['bag.jpg', 'banana.jpg', 'boots.jpg', 'chair.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pen.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 Product.all = [];
-Product.justViewed = [];
+Product.uniqueArray = [];
 Product.btnClearLS = document.getElementById('clear-local-storage');
 Product.container = document.getElementById('image-container');
 Product.tableDynamicEl = document.getElementById('table-dynamic');
@@ -12,7 +12,7 @@ Product.pics = [document.getElementById('left'),
                 document.getElementById('right')];
 Product.totalClicks = 0;
 
-//Product constructor
+//Product constructor, path is concatenating the image folder with the name and extension of the products, then is set as the path
 function Product(name) {
   this.name = name;
   this.path = 'images/' + name;
@@ -26,34 +26,54 @@ for( var i = 0; i < Product.names.length; i++ ) {
   new Product(Product.names[i]);
 }
 
-//creates a random number
+//creates a random number between 0 and 1 exclusive, meaning 1 is above the limit and 0 is below the limit
 var makeRandomNumber = function() {
   return Math.floor(Math.random() * Product.names.length);
 };
 
-//this function ensures that no duplicates are displayed
-var displayPics = function() {
-  var randomImages = [];
+// var displayPics = function() {
+//   //stores 3 unique numbers
+//   var randomImages = [];
+//   //assigns random values to randomImages index 1 and 2
+//   randomImages[0] = makeRandomNumber();
+//   randomImages[1] = makeRandomNumber();
+//   //here we are checking to see if you randomImages at index 1 is equal to index 2, if so reassign index 1 by calling makeRandomNumber()
+//   while(randomImages[0] === randomImages[1]){
+//     console.log('Duplicate Found');
+//     randomImages[1] = makeRandomNumber();
+//   }
+//   //creating the 3rd number in the array and ensuring that it does not match the previous 2 indexes
+//   randomImages[2] = makeRandomNumber();
+//   while(randomImages[2] === randomImages[1] || randomImages[2] === randomImages[0]){
+//     console.log('Duplicate Found');
+//     randomImages[2] = makeRandomNumber();
+//   }
+//   //using the randomImages array numbers to assign the source a path and a name to the Products in the Products.pics array
+//   for( var i = 0; i < 3; i++ ) {
+//     Product.pics[i].src = Product.all[randomImages[i]].path;
+//     Product.pics[i].id = Product.all[randomImages[i]].name;
+//     Product.all[randomImages[i]].views += 1;
+//   }
+// };
 
-  randomImages[0] = makeRandomNumber();
-  randomImages[1] = makeRandomNumber();
-
-  while(randomImages[0] === randomImages[1]){
-    console.log('Duplicate Found');
-    randomImages[1] = makeRandomNumber();
+function displayPics() {
+ //keeps the array filled with 6 unique value, always
+  while(Product.uniqueArray.length < 6) {
+    var random = makeRandomNumber();
+    while(!Product.uniqueArray.includes(random)) {
+      console.log('building uniqueArray:  ', Product.uniqueArray);
+      Product.uniqueArray.push(random);
+    }
   }
-  randomImages[2] = makeRandomNumber();
-  while(randomImages[2] === randomImages[1] || randomImages[2] === randomImages[0]){
-    console.log('Duplicate Found');
-    randomImages[2] = makeRandomNumber();
+  console.log('uniqueArray completed!!: ', Product.uniqueArray);
+  for( var i = 0; i < Product.uniqueArray.length; i++ ) {
+   //value of the first index of the array is removed and set as the variable 'temp' and replaced at each iteration of the loop
+    var temp = Product.uniqueArray.shift();
+    Product.pics[i].src = Product.all[temp].path;
+    Product.pics[i].id = Product.all[temp].name;
+    Product.all[temp].views += 1;
   }
-  //using the randomImages array numbers to assign the source a path and a name to the Products in the Products.pics array
-  for( var i = 0; i < 3; i++ ) {
-    Product.pics[i].src = Product.all[randomImages[i]].path;
-    Product.pics[i].id = Product.all[randomImages[i]].name;
-    Product.all[randomImages[i]].views += 1;
-    Product.justViewed[i] = randomImages[i];
-  }
+  console.log('remaining values in uniqueArray: ', Product.uniqueArray);
 };
 
 //checks to see if totalClicks equals 5
@@ -82,7 +102,7 @@ var handleClick = function(event) {
   for(var i = 0; i < Product.names.length; i++){
     if(event.target.id === Product.all[i].name) {
       Product.all[i].votes += 1;
-      console.log(event.target.id + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views');
+      // console.log(event.target.id + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views');
     }
   }
   //save to localStorage here!
@@ -117,7 +137,7 @@ Product.namesData = [];
 Product.votesData = [];
 Product.viewsData = [];
 
-//data object which is to be passed into the .Bar() which is chained to the new instance
+//data object which is to be passed into the .Bar() method which is chained to the new instance of Chart
 Product.data = {
   labels: Product.namesData,
   datasets: [
